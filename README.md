2f7002ae2ec374ae479ad0546afa6f9a68863314
Kai Raschke <kr@kaju.it>
2018-12-05 11:04:59 +0100

    change require path because of scoped npm package

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ logging of external task client and bunyan as logging engine.
 
 All current settings from camunda-external-task-client-js are supported.
 Variables should be used by setting on the process environment variables.
-
-Through Gitlab CI runner it gets pushed to a private
-npm repository which need to be available for
-other external task microservices.
--- client.js
+++ client.js
@@ -17,7 +17,7 @@ function init(options){
     //If the app is not started by pm2, we will try to inject the env variables from app.json
     if(!process.env.NODE_ENV){
         try{
-            let appJson = require(path.resolve(__dirname, '..', '..') + path.sep + 'app.json'); //Module within node_modules/org.prodig.external.extras
+            let appJson = require(path.resolve(__dirname, '..', '..', '..') + path.sep + 'app.json'); //Module within node_modules/org.prodig.external.extras
             let env = appJson.apps[0].env;
             process.env = Object.assign(process.env, env);
         } catch(ex){ console.log(ex); }
